6231108d6ca7a58c6378700e1f9f9bf2a610a5bf
agnieszkazydek <agnieszkazydek@onet.pl>
2022-05-04 09:27:17 +0200

    Add project files.

---
+++ Tests.cs
@@ -0,0 +1,39 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Threading;
+using Xunit;
+
+
+namespace GoogleMap
+{
+    public class Tests
+
+    {
+        [Fact]
+        public void LoadApplicationPage()
+        {
+            using (IWebDriver driver = new ChromeDriver())
+            {
+
+                driver.Navigate().GoToUrl("https://www.google.pl/maps/");
+                // Thread.Sleep(500000);
+                driver.FindElement(By.CssSelector("div > form")).Click();
+                
+
+
+
+                Thread.Sleep(5000);
+
+                IWebElement searchBox = driver.FindElement(By.Id("searchboxinput"));
+                searchBox.Click();
+                driver.FindElement(By.CssSelector("button[aria-label='Trasa']")).Click();
+                driver.FindElement(By.CssSelector("img[aria-label='Na rowerze']")).Click();
+                driver.FindElement(By.CssSelector("input[aria - label = 'Wybierz punkt początkowy lub kliknij mapę...']")).Click();
+
+
+
+            }
+        }
+    }
+}
