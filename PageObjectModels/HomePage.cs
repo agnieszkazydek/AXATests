d16215b6b1fe867cba7cb854a0ef8517be5bb7ce
agnieszkazydek <agnieszkazydek@onet.pl>
2022-05-05 21:51:37 +0200

    dziala

---
+++ PageObjectModels/HomePage.cs
@@ -0,0 +1,58 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+
+namespace GoogleMap
+{
+    public class HomePage
+    {
+        public IWebDriver driver;
+
+        protected IWebElement Distance => driver.FindElement(By.CssSelector("button[aria-label='Trasa']"));
+        protected IWebElement Chlodna => driver.FindElement(By.CssSelector("#directions-searchbox-0 input"));
+        IWebElement PlacDefilad => driver.FindElement(By.CssSelector("#directions-searchbox-1 input"));
+        IWebElement GoImg => driver.FindElement(By.CssSelector("img[aria-label='Pieszo']"));
+        IWebElement GoingTime => driver.FindElement(By.CssSelector("#section-directions-trip-0 > div.MespJc > div:nth-child(3) > div.XdKEzd > div.Fk3sm.fontHeadlineSmall"));
+        IWebElement GoingDistance => driver.FindElement(By.CssSelector("#section-directions-trip-0 > div.MespJc > div:nth-child(3) > div.XdKEzd > div.ivN21e.tUEI8e.fontBodyMedium"));
+
+        public void Browser()
+        {
+            driver = new ChromeDriver();
+        }
+
+        
+
+        private const string PageUrl = "https://www.google.pl/maps/";
+        private const string PageTitle = "Mapy Google";
+
+
+        
+            
+
+        }
+
+    internal class SetUpAttribute : Attribute
+    {
+    }
+
+    public void NavigateTo()
+        {
+            driver.Navigate().GoToUrl(PageUrl);
+
+            EnsurePageLoaded();
+        }
+
+        public void EnsurePageLoaded()
+        {
+            bool pageHasLoaded = (driver.Url == PageUrl) && (driver.Title == PageTitle);
+
+            if (!pageHasLoaded)
+            {
+                driver.FindElement(By.CssSelector("div > form")).Click();
+            }
+        }
+ 
+    }
+}
---
+++ Tests/TestGoogleMap.cs
@@ -0,0 +1,69 @@
+﻿using OpenQA.Selenium;
+using System;
+using Xunit;
+
+
+
+namespace GoogleMap
+{
+    public class Tests : HomePage
+
+    {
+        private const string MapsUrl = "https://www.google.pl/maps/";
+
+
+    [Fact]
+        public void CheckDistance()
+        {
+ 
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(5);
+            driver.Navigate().GoToUrl(MapsUrl);
+            
+            driver.FindElement(By.CssSelector("div > form")).Click();
+
+            Distance.Click();
+            Chlodna.SendKeys("Chłodna 51 Warszawa" + Keys.Enter);
+
+            driver.FindElement(By.CssSelector("#directions-searchbox-1 input")).SendKeys("Plac Defilad 1" + Keys.Enter);
+
+            driver.FindElement(By.CssSelector("img[aria-label='Pieszo']")).Click();
+
+            IWebElement goingTime = driver.FindElement(By.CssSelector("#section-directions-trip-0 > div.MespJc > div:nth-child(3) > div.XdKEzd > div.Fk3sm.fontHeadlineSmall"));
+                string goingTimeValue = goingTime.Text;
+
+            IWebElement goingDistance =  driver.FindElement(By.CssSelector("#section-directions-trip-0 > div.MespJc > div:nth-child(3) > div.XdKEzd > div.ivN21e.tUEI8e.fontBodyMedium"));
+                string goingDistanceValue = goingDistance.Text;
+
+            // ----------------- 
+
+            driver.FindElement(By.CssSelector("img[aria-label='Na rowerze']")).Click();
+
+            IWebElement bicycleTime = driver.FindElement(By.CssSelector("#section-directions-trip-0 > div.MespJc > div:nth-child(3) > div.XdKEzd > div.Fk3sm.fontHeadlineSmall"));
+                string bicycleTimeValue = bicycleTime.Text;
+
+
+            IWebElement bicycleDistance = driver.FindElement(By.CssSelector("#section-directions-trip-0 > div.MespJc > div:nth-child(3) > div.XdKEzd > div.ivN21e.tUEI8e.fontBodyMedium"));
+                string bicycleDistanceValue = bicycleDistance.Text;
+
+            driver.FindElement(By.ClassName("reverse")).Click();
+
+            driver.FindElement(By.CssSelector("img[aria-label='Pieszo']")).Click();
+
+
+            string reserveGoingTimeValue = goingTime.Text;
+            string reserveGoingDistanceValue = goingDistance.Text;
+
+
+            driver.FindElement(By.CssSelector("img[aria-label='Na rowerze']")).Click();
+
+            string reserveBicycleTimeValue = bicycleTime.Text;
+            string reserveBicycleDistanceValue = bicycleDistance.Text;
+
+            Assert.True(Convert.ToInt32(bicycleTimeValue) < 40 && Convert.ToInt32(bicycleDistanceValue)  < 3);
+            Assert.True(Convert.ToInt32(bicycleTimeValue) < 40 && Convert.ToInt32(bicycleDistanceValue) < 3);
+            Assert.True(Convert.ToInt32(reserveGoingTimeValue) < 40 && Convert.ToInt32(reserveGoingDistanceValue) < 3);
+            Assert.True(Convert.ToInt32(reserveBicycleTimeValue) < 40 && Convert.ToInt32(reserveBicycleDistanceValue) < 3);
+
+        }
+    }
+}
